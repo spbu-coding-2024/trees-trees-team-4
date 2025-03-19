30a9a22a0b62f2864c5ae9d48b550440dcd1b522
Roman Maltsev <roman.maltsev.realgar@proton.me>
2025-03-19 21:36:18 +0300

    fix: Changed types in insert and iterator methods

--- lib/src/main/kotlin/org/treelib/BinaryTree.kt
+++ lib/src/main/kotlin/org/treelib/BinaryTree.kt
@@ -6,9 +6,9 @@ abstract class BinaryTree<K: Comparable<K>, V: Any>(var key: K, var data: V) {
     internal var root: Node<K, V>? = null
     abstract fun min(root: Node<K, V>?): K
     abstract fun max(root: Node<K, V>?): K
-    abstract fun insert(value: V): Node<K, V>? // what should it return?
+    abstract fun insert(key: K, value: V): Node<K, V>?
     abstract fun search(key: K): Node<K, V>?
     abstract fun delete(key: K): Node<K, V>?
-    abstract fun iterator(key: K): Node<K, V>? // what should it return?
+    abstract fun iterator(key: K): Iterable<Node<K, V>>
 
 }
