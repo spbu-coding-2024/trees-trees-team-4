b7574c824e64b08482cdad180a3c98b11056bdf0
pechenb <romkor2006@gmail.com>
2025-03-20 10:30:51 +0300

    fix: switch to tabs & compatible generic types

--- lib/src/main/kotlin/org/treelib/BinaryTree.kt
+++ lib/src/main/kotlin/org/treelib/BinaryTree.kt
@@ -3,12 +3,11 @@ package org.treelib
 
 
 abstract class BinaryTree<K: Comparable<K>, V: Any>(var key: K, var data: V) {
-    internal var root: Node<K, V>? = null
-    abstract fun min(root: Node<K, V>?): K
-    abstract fun max(root: Node<K, V>?): K
-    abstract fun insert(key: K, value: V): Node<K, V>?
-    abstract fun search(key: K): Node<K, V>?
-    abstract fun delete(key: K): Node<K, V>?
-    abstract fun iterator(key: K): Iterable<Node<K, V>>
-
+	internal var root: Node<K, V>? = null
+	abstract fun min(root: Node<K, V>?): K
+	abstract fun max(root: Node<K, V>?): K
+	abstract fun insert(key: K, value: V): Node<K, V>?
+	abstract fun search(key: K): Node<K, V>?
+	abstract fun delete(key: K): Node<K, V>?
+	abstract fun iterator(key: K): Iterable<Node<K, V>>
 }
