cdb69617a049a98cf68323c39c11ce267dc91212
samodurov.nikita <nikita98351@gmail.com>
2025-03-20 09:16:44 +0300

    feat: rotatabletree implementation

--- lib/src/main/kotlin/org/treelib/RotatableTree.kt
+++ lib/src/main/kotlin/org/treelib/RotatableTree.kt
@@ -1,6 +1,19 @@
 package org.treelib
 
 interface RotatableTree<K: Comparable<K>, V> {
-	fun rotateLeft(node: Node<K, V>)
-	fun rotateRight(node: Node<K, V>)
+  fun rotateLeft(node: Node<K, V>) {
+    requireNotNull(node.right)
+    val x = node.right
+    node.right = x.left
+    x.left = node
+    return x
+  }
+
+  fun rotateRight(node: Node<K, V>) {
+    requireNotNull(node.left)
+    val x = node.left
+    node.left = x.right
+    x.right = node
+    return x
+  }
 }
