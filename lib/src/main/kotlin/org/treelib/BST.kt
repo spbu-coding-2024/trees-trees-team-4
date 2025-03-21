ddf0dc6a71341f7b7972b42f01733fb8bab4a61b
Roman Maltsev <roman.maltsev.realgar@proton.me>
2025-03-21 11:27:35 +0300

    refactor: min() and max() moved to abstract class BinaryTree

--- lib/src/main/kotlin/org/treelib/BST.kt
+++ lib/src/main/kotlin/org/treelib/BST.kt
@@ -1,21 +1,6 @@
 package org.treelib
 
 class BST<K: Comparable<K>, V: Any>(key: K, data: V) : BinaryTree<K, V>(key, data) {
-    override fun min(root: Node<K, V>?): K {
-        var resultNode = root ?: throw NoSuchElementException("Tree is empty")
-        while (resultNode.left != null) {
-            resultNode = resultNode.left!!
-        }
-        return resultNode.key
-    }
-
-    override fun max(root: Node<K, V>?): K {
-        var resultNode = root ?: throw NoSuchElementException("Tree is empty")
-        while (resultNode.right != null) {
-            resultNode = resultNode.right!!
-        }
-        return resultNode.key
-    }
 
     override fun insert(key: K, value: V): Node<K, V>? {
         TODO("Not yet implemented")
--- lib/src/main/kotlin/org/treelib/BinaryTree.kt
+++ lib/src/main/kotlin/org/treelib/BinaryTree.kt
@@ -4,8 +4,22 @@ package org.treelib
 
 abstract class BinaryTree<K: Comparable<K>, V: Any>(var key: K, var data: V) {
     internal var root: Node<K, V>? = null
-    abstract fun min(root: Node<K, V>?): K
-    abstract fun max(root: Node<K, V>?): K
+    fun min(): K? {
+        var resultNode = root ?: return null
+        while (resultNode.left != null) {
+            resultNode = resultNode.left!!
+        }
+        return resultNode.key
+    }
+
+    fun max(): K? {
+        var resultNode = root ?: return null
+        while (resultNode.right != null) {
+            resultNode = resultNode.right!!
+        }
+        return resultNode.key
+    }
+
     abstract fun insert(key: K, value: V): Node<K, V>?
     abstract fun search(key: K): Node<K, V>?
     abstract fun delete(key: K): Node<K, V>?
