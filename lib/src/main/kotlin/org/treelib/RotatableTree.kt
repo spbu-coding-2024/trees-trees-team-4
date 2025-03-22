8107f37543672f8b1a96356a0f4ed075a82e029b
samodurov.nikita <nikita98351@gmail.com>
2025-03-22 12:02:52 +0300

    test: RBTree black balance invariant and insert function

--- lib/src/main/kotlin/org/treelib/RotatableTree.kt
+++ lib/src/main/kotlin/org/treelib/RotatableTree.kt
@@ -1,24 +1,24 @@
 package org.treelib
 
-interface RotatableTree<K: Comparable<K>, V : Any> {
-  fun rotateLeft(node: Node<K, V>): Node<K, V> {
+interface RotatableTree<K: Comparable<K>, V : Any, N: Node<K, V, N>> {
+  fun rotateLeft(node: N): N {
     val x = node.right
     x?.let {
-      node.right = x.left
-      x.left = node
-      return x
+      node.right = it.left
+      it.left = node
+      return it
     } ?: let {
-      throw NullPointerException("Can't rotate left: right node is null")
+      throw NoSuchElementException("Can't rotate left: right node is null")
     }
   }
-  fun rotateRight(node: Node<K, V>): Node<K, V> {
+  fun rotateRight(node: N): N {
     val x = node.left
     x?.let {
       node.left = x.right
       x.right = node
       return x
     } ?: let {
-      throw NullPointerException("Can't rotate right: left node is null")
+      throw NoSuchElementException("Can't rotate right: left node is null")
     }
 
   }
--- lib/src/test/kotlin/org/treelib/RedBlackTreeTests.kt
+++ lib/src/test/kotlin/org/treelib/RedBlackTreeTests.kt
@@ -1,2 +1,92 @@
 package org.treelib
 
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.RepeatedTest
+import org.junit.platform.commons.logging.Logger
+import java.lang.annotation.Repeatable
+import kotlin.math.abs
+import kotlin.random.Random
+import kotlin.test.BeforeTest
+
+class RBTreeInvariantCheck<K: Comparable<K>, V: Any>(var tree: RedBlackTree<K, V>) {
+
+    fun isBlackBalanced(): Boolean {
+        return subtreeBlackCountDifference(tree.root) != -1
+    }
+
+    private fun subtreeBlackCountDifference(node: RBNode<K, V>?): Int {
+        node?.let {
+            var left = subtreeBlackCountDifference(node.left)
+            var right = subtreeBlackCountDifference(node.right)
+            if (left == right) {
+                return left
+            } else {
+                return -1
+            }
+
+        } ?:let {
+            return 1
+        }
+    }
+}
+
+class RedBlackTreeTests {
+    lateinit var intTree: RedBlackTree<Int, Int>
+    lateinit var check: RBTreeInvariantCheck<Int, Int>
+    @BeforeTest
+    fun before() {
+        intTree = RedBlackTree<Int, Int>()
+        check = RBTreeInvariantCheck<Int, Int>(intTree)
+    }
+    @Test
+    fun `assert root is null right after initialization`() {
+        assertNull(intTree.root)
+    }
+
+    @Test
+    fun `insert 1`() {
+        intTree.insert(1,1)
+        assertEquals(intTree.root?.data, 1)
+        assertEquals(intTree.root?.key, 1)
+    }
+
+    @Test
+    fun `insert 2`() {
+        intTree.insert(2, 257)
+        intTree.insert(1, 256)
+        intTree.insert(3, 247)
+        assertEquals(intTree.root?.data, 257)
+        assertEquals(intTree.root?.left?.data, 256)
+        assertEquals(intTree.root?.right?.data, 247)
+    }
+
+    @Test
+    fun `is perfect black balanced 1`() {
+        intTree.insert(10, 1)
+        intTree.insert(44, 2)
+        intTree.insert(3, 3)
+        intTree.insert(88, 4)
+        intTree.insert(2, 5)
+        intTree.insert(86, 6)
+        intTree.insert(20, 7)
+        intTree.insert(60, 8)
+        var bool = check.isBlackBalanced()
+        assertEquals(bool, true)
+    }
+
+    @RepeatedTest(10)
+    fun `is perfect black balanced 2`() {
+        for (i in 1..25) {
+            var randomKey = Random.nextInt(0,1000)
+            intTree.insert(randomKey, i)
+            println("intTree.insert($randomKey, $i)")
+        }
+
+        assertEquals(check.isBlackBalanced(), true)
+    }
+
+
+
+}
