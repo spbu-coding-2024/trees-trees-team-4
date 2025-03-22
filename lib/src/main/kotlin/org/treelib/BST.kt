952a23e7bac9e26c79442030a2a96864ee639808
Roman Maltsev <roman.maltsev.realgar@proton.me>
2025-03-22 14:00:44 +0300

    feat: added overriding of insert in BST

--- lib/src/main/kotlin/org/treelib/BST.kt
+++ lib/src/main/kotlin/org/treelib/BST.kt
@@ -6,8 +6,28 @@ class BST<K: Comparable<K>, V: Any>(key: K, data: V) : BinaryTree<K, V>(key, dat
 
     override var root: Node<K, V>? = BSTNode(key, data)
 
-    override fun insert(key: K, value: V): Node<K, V>? {
-        TODO("Not yet implemented")
+    override fun insert(key: K, value: V): Node<K, V> {
+        val resultNode: BSTNode<K, V> = BSTNode(key, value)
+        var currentNode: BSTNode<K, V>? = root as BSTNode<K, V>?
+        while (currentNode?.key != null) {
+            currentNode =
+                if (currentNode.key > resultNode.key) {
+                    (currentNode.left ?: break) as BSTNode<K, V>?
+                } else {
+                    (currentNode.right ?: break) as BSTNode<K, V>?
+            }
+        }
+        if (currentNode != null) {
+            if (currentNode.key > resultNode.key) {
+                currentNode.left = resultNode
+            } else {
+                currentNode.right = resultNode
+            }
+        }
+        else{
+            root = resultNode
+        }
+        return resultNode
     }
 
     override fun search(key: K): Node<K, V>? {
