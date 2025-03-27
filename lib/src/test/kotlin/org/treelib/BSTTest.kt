12b4244ff3cb46a936d0dd0f6460a17a36496389
Roman Maltsev <roman.maltsev.realgar@proton.me>
2025-03-27 21:42:44 +0300

    feat: added min/max tests carcass

---
+++ lib/src/test/kotlin/org/treelib/BSTTest.kt
@@ -0,0 +1,48 @@
+package org.treelib
+
+import org.junit.jupiter.api.Test
+
+class BSTTest {
+    @Test
+    fun `min of empty tree`(){
+        TODO("Not yet implemented")
+    }
+    @Test
+    fun `max of empty tree`(){
+        TODO("Not yet implemented")
+    }
+    @Test
+    fun `is min of single node tree = max of single node tree`(){
+        TODO("Not yet implemented")
+    }
+    @Test
+    fun `simple min (int)`(){
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `simple max (int)`(){
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `simple min (string)`(){
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `simple max (string)`(){
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `simple min (Node Int, Int)`(){
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `simple max (Node Int, Int)`(){
+        TODO("Not yet implemented")
+    }
+
+}
