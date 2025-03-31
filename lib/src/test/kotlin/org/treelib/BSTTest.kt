cf27ecd1b7990e6a94d0a27fe45bca5bd074e3c6
Roman Maltsev <roman.maltsev.realgar@proton.me>
2025-03-31 16:23:08 +0300

    feat: WIP added single node tree tests for BST

--- lib/src/test/kotlin/org/treelib/BSTTest.kt
+++ lib/src/test/kotlin/org/treelib/BSTTest.kt
@@ -16,17 +16,36 @@ class BSTTest {
     @Test
     fun `insert to empty tree`(){
         val tree: BST<Int, Int> = BST()
-        assert(tree.findMax() == null)
+        val node: BSTNode<Int, Int> = tree.insert(0, 0)
+        assert(tree.root == node)
     }
     @Test
     fun `delete from empty tree`(){
         val tree: BST<Int, Int> = BST()
-        assert(tree.findMax() == null)
+        assert(tree.delete(0) == null)
     }
     @Test
     fun `is min of single node tree = max of single node tree`(){
         val tree: BST<Int, Int> = BST(BSTNode(0, 0))
         assert(tree.findMin() == tree.findMax())
+    }
+    @Test
+    fun `insert to single node tree`(){
+        val tree: BST<Int, Int> = BST(BSTNode(1, 1))
+        val node = tree.insert(0, 0)
+        assert(tree.root?.left == node)
+    }
+    @Test
+    fun `is delete from single node tree returning required node`(){
+        val node: BSTNode<Int, Int> = BSTNode(1, 1)
+        val tree: BST<Int, Int> = BST(node)
+        assert(tree.delete(1) == node)
+    }
+    @Test
+    fun `is delete from single node tree actually deleting required node`(){
+        val node: BSTNode<Int, Int> = BSTNode(1, 1)
+        val tree: BST<Int, Int> = BST(node)
+        assert(tree.delete(1) == node)
     }
     @Test
     fun `simple min (int)`(){
