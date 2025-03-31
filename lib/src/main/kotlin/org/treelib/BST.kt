ca3d5c531b89c7949e23f997f5ae16cee085a756
Roman Maltsev <roman.maltsev.realgar@proton.me>
2025-03-31 15:55:38 +0300

    refactor: BST reworked under detekt

--- lib/src/main/kotlin/org/treelib/BST.kt
+++ lib/src/main/kotlin/org/treelib/BST.kt
@@ -9,12 +9,11 @@ class BST<K : Comparable<K>, V : Any>(override var root: BSTNode<K, V>? = null) 
         val resultNode: BSTNode<K, V> = BSTNode(key, data)
         var currentNode: BSTNode<K, V>? = root
         while (currentNode?.key != null) {
-            currentNode =
-                if (currentNode.key > resultNode.key) {
-                    (currentNode.left ?: break)
-                } else {
-                    (currentNode.right ?: break)
-                }
+            currentNode = if (currentNode.key > resultNode.key) {
+                currentNode.left
+            } else {
+                currentNode.right
+            } ?: break
         }
         if (currentNode != null) {
             if (currentNode.key > resultNode.key) {
@@ -30,68 +29,42 @@ class BST<K : Comparable<K>, V : Any>(override var root: BSTNode<K, V>? = null) 
 
     override fun delete(key: K): BSTNode<K, V>? {
         var result: BSTNode<K, V>? = root
-        var targetNode: BSTNode<K, V>? = root
-        var currentNode: BSTNode<K, V>? = null
-        val minimum: BSTNode<K, V>?
-        while (targetNode != null) {
+        var target: BSTNode<K, V>? = root
+        var current: BSTNode<K, V>? = null
+        while (target != null) {
             // Если нашли нужную ноду
-            if (targetNode.key == key) {
-                result = targetNode
-                if (currentNode == null) {
-                    // Если currentNode == null, то из корня мы никуда не ушли, проверяем корень
-                    root =
-                        if (targetNode.right == null) {
-                            targetNode.left
-                        } else if (targetNode.left == null) {
-                            targetNode.right
-                        } else {
-                            minimum = findMin(targetNode.right)
-                            if (minimum != null) {
-                                minimum.left = targetNode.left
-                            }
-                            targetNode.right
-                        }
-                } else {
-                    // Проверяем, каким из потомков является результирующая.
-                    if (currentNode.left == targetNode) {
-                        // Подменяем результирующую на ее потомка
-                        currentNode.left =
-                            if (targetNode.right == null) {
-                                targetNode.left
-                            } else if (targetNode.left == null) {
-                                targetNode.right
-                            } else {
-                                minimum = findMin(targetNode.right)
-                                if (minimum != null) {
-                                    minimum.left = targetNode.left
-                                }
-                                targetNode.right
-                            }
-                    } else {
-                        // Подменяем результирующую на ее потомка
-                        currentNode.right =
-                            if (targetNode.right == null) {
-                                targetNode.left
-                            } else if (targetNode.left == null) {
-                                targetNode.right
-                            } else {
-                                minimum = findMin(targetNode.right)
-                                if (minimum != null) {
-                                    minimum.left = targetNode.left
-                                }
-                                targetNode.right
-                            }
-                    }
+            if (target.key == key) {
+                result = target
+                if (current == null) {
+                    // Если currentNode == null, то из корня мы никуда не ушли
+                    root = replaceWithAppending(target, findMin(target.right))
+                } else if (current.left == target) {
+                    current.left = replaceWithAppending(target, findMin(target.right))
                 }
+                else
+                    current.right = replaceWithAppending(target, findMin(target.right))
                 break;
             }
-            if (targetNode.key > key) {
-                currentNode = targetNode
-                targetNode = targetNode.left ?: break
-            } else if (targetNode.key < key) {
-                currentNode = targetNode
-                targetNode = targetNode.right ?: break
+            current = target
+            target = if (target.key > key) {
+                target.left
+            } else {
+                target.right
             }
+        }
+        return result
+    }
+
+    private fun replaceWithAppending(target: BSTNode<K, V>, required: BSTNode<K, V>?): BSTNode<K, V>? {
+        val result: BSTNode<K, V>? = if (target.right == null) {
+            target.left
+        } else if (target.left == null) {
+            target.right
+        } else {
+            if (required != null) {
+                required.left = target.left
+            }
+            target.right
         }
         return result
     }
