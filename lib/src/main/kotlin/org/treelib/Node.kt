d99c17ac4ca9761141631019eaccf860e8158d09
pechenb <romkor2006@gmail.com>
2025-04-05 18:30:39 +0300

    refactor: Node class D generic name instead of V

--- lib/src/main/kotlin/org/treelib/Node.kt
+++ lib/src/main/kotlin/org/treelib/Node.kt
@@ -2,7 +2,7 @@
 package org.treelib
 
 
-abstract class Node<K: Comparable<K>, V: Any, N: Node<K, V, N>>(var key: K, var data: V) {
+abstract class Node<K: Comparable<K>, D: Any?, N: Node<K, D, N>>(var key: K, var data: D?) {
     internal var left: N? = null
     internal var right: N? = null
 }
