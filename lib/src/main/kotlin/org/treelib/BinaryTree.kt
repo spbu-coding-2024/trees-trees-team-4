6abf79fcb3069290705ef9b9dcc2afd82d5063f4
pechenb <romkor2006@gmail.com>
2025-04-06 06:48:54 +0300

    docs: added docs for BinaryTree

--- lib/src/main/kotlin/org/treelib/BinaryTree.kt
+++ lib/src/main/kotlin/org/treelib/BinaryTree.kt
@@ -1,9 +1,23 @@
 package org.treelib
 
+/**
+ * An abstract binary tree that provides basic operations for finding minimum and maximum data,
+ * searching for a key, and iterating over the data.
+ *
+ * @param K the type of keys maintained by this tree.
+ * @param D the type of mapped data (nullable).
+ * @param N the type of node used in the tree.
+ */
 abstract class BinaryTree<K : Comparable<K>, D : Any?, N : Node<K, D?, N>> {
 	var root: N? = null
 		protected set
 
+	/**
+	 * Returns the minimum data in the tree starting from the specified node.
+	 *
+	 * @param start the node from which to start the search (defaults to the root).
+	 * @return the minimum data value, or null if the tree is empty.
+	 */
 	fun findMin(start: N? = root): D? {
 		var resultNode = start ?: return null
 		while (true) {
@@ -11,6 +25,12 @@ abstract class BinaryTree<K : Comparable<K>, D : Any?, N : Node<K, D?, N>> {
 		}
 	}
 
+	/**
+	 * Returns the maximum data in the tree starting from the specified node.
+	 *
+	 * @param start the node from which to start the search (defaults to the root).
+	 * @return the maximum data value, or null if the tree is empty.
+	 */
 	fun findMax(start: N? = root): D? {
 		var resultNode = start ?: return null
 		while (true) {
@@ -18,13 +38,20 @@ abstract class BinaryTree<K : Comparable<K>, D : Any?, N : Node<K, D?, N>> {
 		}
 	}
 
-	protected fun findPredecessor(node: N): N?{
+	protected fun findPredecessor(node: N): N? {
 		var resultNode = node.left ?: return null
 		while (true) {
 			resultNode = resultNode.right ?: return resultNode
 		}
 	}
 
+	/**
+	 * Searches for a node with the specified key starting from the given node.
+	 *
+	 * @param key the key to search for.
+	 * @param start the node from which to start the search (defaults to the root).
+	 * @return the data associated with the node, or null if no such node is found.
+	 */
 	fun search(key: K, start: N? = root): D? {
 		var node = start
 		while (node != null) {
@@ -37,8 +64,28 @@ abstract class BinaryTree<K : Comparable<K>, D : Any?, N : Node<K, D?, N>> {
 		return null
 	}
 
+	/**
+	 * Inserts a new node with the specified key and data into the tree.
+	 * If a node with the same key already exists, its data is updated.
+	 *
+	 * @param key the key of the node to insert.
+	 * @param data the data associated with the key.
+	 */
 	abstract fun insert(key: K, data: D?)
+
+	/**
+	 * Deletes the node with the specified key from the tree.
+	 * If the node is not found, an exception is thrown.
+	 *
+	 * @param key the key of the node to delete.
+	 */
 	abstract fun delete(key: K)
+
+	/**
+	 * Returns an iterator over the data in the tree in in-order traversal.
+	 *
+	 * @return an iterator over the tree's data.
+	 */
 	operator fun iterator(): Iterator<D?> = iterator {
 		inorder(root)
 	}
@@ -51,10 +98,20 @@ abstract class BinaryTree<K : Comparable<K>, D : Any?, N : Node<K, D?, N>> {
 		}
 	}
 
+	/**
+	 * Returns the next data from the iterator.
+	 *
+	 * @return the next data, or null if there are no more elements.
+	 */
 	fun next(): D? {
 		return iterator().next()
 	}
 
+	/**
+	 * Checks if there are more data available in the iterator.
+	 *
+	 * @return true if there is at least one more element, false otherwise.
+	 */
 	fun hasNext(): Boolean {
 		return iterator().hasNext()
 	}
